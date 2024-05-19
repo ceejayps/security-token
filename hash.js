da45cb5949a0e65c1376ee2ae0a310c9ac6bad96
Chester Johnson <59537158+ceejayps@users.noreply.github.com>
2024-05-19 11:40:15 -0500

    Update hash.js

--- hash.js
+++ hash.js
@@ -1,9 +1,28 @@
 const crypto = require("crypto");
 const tokenId = 1876;
 
+// Get the current date and time
+let now = new Date();
+
+// Format the date and time
+let options = {
+  timeZone: "America/New_York", // You can change this to the desired US timezone
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  hour12: true, // Use 12-hour clock format
+};
+
+// Get the formatted date and time string
+
 const genr8Code = (id) => {
+  let formattedTime = now.toLocaleString("en-US", options);
   // Get the current time without seconds
-  const time = new Date().toISOString().slice(0, 16);
+  const time = formattedTime;
+  console.log(time);
   // Covert the ios string to time string
   const hashTime = Date.parse(time);
   // Combine the id and current time into a single string
@@ -21,7 +40,7 @@ const genr8Code = (id) => {
   // Convert hexstring to decimal
   const code = parseInt(hexCode, 16);
   //return parseInt(code, 16);
-  return code.toString().substring(0, 6)
+  return code.toString().substring(0, 6);
 };
 
 console.log(genr8Code(tokenId));
